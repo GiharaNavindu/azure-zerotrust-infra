7b22f2fbd0838726ca44e3732d1d5f1e0711c41f
GiharaNavindu <rathnaweera_pagn_e23@engug.ruh.ac.lk>
2026-07-17 01:36:57 +0530

    configure azurerm remote state backend

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ terraform {
     }
   }
   required_version = ">= 1.5.0"
+
+
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate-mgmt"
+    storage_account_name = "sttfstatesttfstate84719" 
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
